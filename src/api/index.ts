18130440c24fd25e232661206787530a28987e43
Monki-D-luffy <2891608692@qq.com>
2025-11-05 16:25:36 +0800

    1. 修改后端服务链接为 http://localhost:3000/api 2. 代码运行成功了,终于搞定json-server了

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,8 @@ import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import router from '@/router' // 我们需要 router 来实现 "401 踢回登录页"
 
-const API_BASE_URL = 'http://192.168.1.100/api'
+const API_BASE_URL = 'http://localhost:3000/api'
+
 const api = axios.create({
   baseURL: API_BASE_URL
 })
