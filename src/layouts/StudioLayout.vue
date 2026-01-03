87161b0ee6fa8b44a452bb787e0c790e0422a7f6
Monki-D-luffy <2891608692@qq.com>
2026-01-03 16:26:24 +0800

    解决侧边栏打开后,右侧空间被挤压出去了, 无法在屏幕中看到

--- src/layouts/StudioLayout.vue
+++ src/layouts/StudioLayout.vue
@@ -134,7 +134,7 @@ const handleStepClick = (step: any) => {
 
 .studio-layout {
   height: 100vh;
-  width: 100vw;
+  /* width: 100vw; */
   background-color: var(--bg-canvas, #f5f7fa);
   display: flex;
   flex-direction: column;
